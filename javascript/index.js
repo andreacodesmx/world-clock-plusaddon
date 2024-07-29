ccb19b58f32fc8864941778a6638c5347a51972f
Andrea Hernández <72625509+andreacodesmx@users.noreply.github.com>
2024-07-28 20:48:55 -0600

    Setting of select elements

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,34 @@
 function updateSeconds() {
-//Merida
-let meridaElement = document.querySelector("#merida");
-let meridaDateElement = meridaElement.querySelector(".date");
-let meridaTimeElement = meridaElement.querySelector(".time");
-let meridaTime = moment().tz("America/Merida");
-meridaDateElement.innerHTML = meridaTime.format("dddd, MMMM Do, YYYY");
-meridaTimeElement.innerHTML = meridaTime.format("h:mm:ss [<small>]A[</small>]");
+  //Merida
+  let meridaElement = document.querySelector("#merida");
+  let meridaDateElement = meridaElement.querySelector(".date");
+  let meridaTimeElement = meridaElement.querySelector(".time");
+  let meridaTime = moment().tz("America/Merida");
+  meridaDateElement.innerHTML = meridaTime.format("dddd, MMMM Do, YYYY");
+  meridaTimeElement.innerHTML = meridaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-//Ljubljana
-let ljubljanaElement = document.querySelector("#ljubljana");
-let ljubljanaDateElement = ljubljanaElement.querySelector(".date");
-let ljubljanaTimeElement =ljubljanaElement.querySelector(".time");
-let ljubljanaTime = moment().tz("Europe/Ljubljana");
-ljubljanaDateElement.innerHTML = ljubljanaTime.format("dddd, MMMM Do, YYYY");
-ljubljanaTimeElement.innerHTML = ljubljanaTime.format("h:mm:ss [<small>]A[</small>]");
-
-//Tijuana
-let tijuanaElement = document.querySelector("#tijuana");
-let tijuanaDateElement = tijuanaElement.querySelector(".date");
-let tijuanaTimeElement = tijuanaElement.querySelector(".time");
-let tijuanaTime = moment().tz("America/Tijuana");
-tijuanaDateElement.innerHTML = tijuanaTime.format("dddd, MMMM Do, YYYY");
-tijuanaTimeElement.innerHTML = tijuanaTime.format("h:mm:ss [<small>]A[</small>]");
-
-//Istanbul
-let istanbulElement = document.querySelector("#istanbul");
-let istanbulDateElement = istanbulElement.querySelector(".date");
-let istanbulTimeElement = istanbulElement.querySelector(".time");
-let istanbulTime = moment().tz("Asia/Istanbul");
-istanbulDateElement.innerHTML = istanbulTime.format("dddd, MMMM Do, YYYY");
-istanbulTimeElement.innerHTML = istanbulTime.format("h:mm:ss [<small>]A[</small>]");
-
-//Chicago
-let chicagoElement = document.querySelector("#chicago");
-let chicagoDateElement = chicagoElement.querySelector(".date");
-let chicagoTimeElement = chicagoElement.querySelector(".time");
-let chicagoTime = moment().tz("America/Chicago");
-chicagoDateElement.innerHTML = chicagoTime.format("dddd, MMMM Do, YYYY");
-chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+            <h2>${cityName}</h2>
+            <div class="info">
+              <div class="time">${cityTime.format(
+                "h:mm:ss [<small>]A[</small>]"
+              )}</div>
+              <div class="date">${cityTime.format("dddd, MMMM Do, YYYY")}</div>
+            </div>
+          </div>
+          `;
 }
 updateSeconds();
 setInterval(updateSeconds, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
